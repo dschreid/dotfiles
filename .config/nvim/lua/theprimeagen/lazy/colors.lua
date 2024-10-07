b0f03922e80764acce61b06356e1f456e47b6e63
dschreid <dominik.schreider@yahoo.com>
2024-10-07 13:24:32 +0200

    java colors, java lombok attempt, treesitter max lines

--- .config/nvim/lua/theprimeagen/lazy/colors.lua
+++ .config/nvim/lua/theprimeagen/lazy/colors.lua
@@ -6,19 +6,23 @@ return {
             vim.o.termguicolors = true
             vim.cmd([[colorscheme ayu]])
 
-            vim.api.nvim_set_hl(0, "@lsp.type.namespace", { fg = "#e5e5e5" }) -- Color for module paths in imports
-            vim.api.nvim_set_hl(0, "@lsp.type.struct.rust", { fg = "#f6993f" }) -- Color for module paths in imports
+            vim.api.nvim_set_hl(0, "@lsp.type.namespace", { fg = "#e5e5e5" })      -- Color for module paths in imports
+            vim.api.nvim_set_hl(0, "@lsp.type.struct.rust", { fg = "#f6993f" })    -- Color for module paths in imports
             vim.api.nvim_set_hl(0, "@lsp.type.interface.rust", { fg = "#f6993f" }) -- Color for module paths in imports
-            vim.api.nvim_set_hl(0, "Operator", { fg = "#e5e5e5" }) -- Color for module paths in imports
 
+            vim.api.nvim_set_hl(0, "@lsp.type.namespace.java", { fg = "#e5e5e5" })
+            vim.api.nvim_set_hl(0, "@lsp.type.class.java", { fg = "#f6993f" })
+            vim.api.nvim_set_hl(0, "@lsp.type.interface.java", { fg = "#f6993f" })
+
+            vim.api.nvim_set_hl(0, "Operator", { fg = "#e5e5e5" })                 -- Color for module paths in imports
         end
     },
 
     {
         "arturgoms/moonbow.nvim",
         config = function()
---            vim.o.termguicolors = true
---            vim.cmd([[colorscheme moonbow]])
+            --            vim.o.termguicolors = true
+            --            vim.cmd([[colorscheme moonbow]])
         end
     },
     {
--- .config/nvim/lua/theprimeagen/lazy/java.lua
+++ .config/nvim/lua/theprimeagen/lazy/java.lua
@@ -1,23 +1,40 @@
 return {
-  "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            -- your jdtls configuration goes here
-          },
+    "nvim-java/nvim-java",
+    config = false,
+    dependencies = {
+        {
+            "neovim/nvim-lspconfig",
+            opts = {
+                servers = {
+                    jdtls = {
+                        -- your jdtls configuration goes here
+                        -- cmd = {
+                        --     'java',
+                        --     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                        --     '-Dosgi.bundles.defaultStartLevel=4',
+                        --     '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                        --     '-Dlog.protocol=true',
+                        --     '-Dlog.level=ALL',
+                        --     '-Xms4g',
+                        --     '--add-modules=ALL-SYSTEM',
+                        --     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+                        --     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+                        --     '-javaagent:' .. os.getenv("HOME") .. '/Coding/libs/lombok.jar',
+                        --     '-jar', os.getenv("HOME") .. '/Coding/libs/jdt/plugins/org.eclipse.equinox.launcher.jar',
+                        --     '-configuration', os.getenv("HOME") .. '/Coding/libs/jdt/config\_linux',
+                        --     '-data', workspace_dir
+                        --
+                        -- },
+                    },
+                },
+                setup = {
+                    jdtls = function()
+                        require("java").setup({
+                            -- your nvim-java configuration goes here
+                        })
+                    end,
+                },
+            },
         },
-        setup = {
-          jdtls = function()
-            require("java").setup({
-              -- your nvim-java configuration goes here
-            })
-          end,
-        },
-      },
     },
-  },
 }
