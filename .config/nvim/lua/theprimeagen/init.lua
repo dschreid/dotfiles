29ca206307a24d2cecc261a354c6f2b7aa64f20a
dschreid <dominik.schreider@yahoo.com>
2024-11-09 01:00:00 +0100

    Disabled Lsp Semantic Tokens

--- .config/nvim/lua/theprimeagen/init.lua
+++ .config/nvim/lua/theprimeagen/init.lua
@@ -41,11 +41,18 @@ autocmd('TextYankPost', {
     end,
 })
 
-autocmd({"BufWritePre"}, {
+autocmd({ "BufWritePre" }, {
     group = ThePrimeagenGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
+
+autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+    end,
+});
 
 autocmd('LspAttach', {
     group = ThePrimeagenGroup,
--- .config/nvim/lua/theprimeagen/lazy/colors.lua
+++ .config/nvim/lua/theprimeagen/lazy/colors.lua
@@ -49,15 +49,15 @@ return {
         "ayu-theme/ayu-vim",
         priority = -100,
         config = function()
-            vim.api.nvim_set_hl(0, "@lsp.type.namespace", { fg = "#e5e5e5" })      -- Color for module paths in imports
-            vim.api.nvim_set_hl(0, "@lsp.type.struct.rust", { fg = "#f6993f" })    -- Color for module paths in imports
-            vim.api.nvim_set_hl(0, "@lsp.type.interface.rust", { fg = "#f6993f" }) -- Color for module paths in imports
-
-            vim.api.nvim_set_hl(0, "@lsp.type.namespace.java", { fg = "#e5e5e5" })
-            vim.api.nvim_set_hl(0, "@lsp.type.class.java", { fg = "#f6993f" })
-            vim.api.nvim_set_hl(0, "@lsp.type.interface.java", { fg = "#f6993f" })
-
-            vim.api.nvim_set_hl(0, "Operator", { fg = "#e5e5e5" }) -- Color for module paths in imports
+            -- vim.api.nvim_set_hl(0, "@lsp.type.namespace", { fg = "#e5e5e5" })      -- Color for module paths in imports
+            -- vim.api.nvim_set_hl(0, "@lsp.type.struct.rust", { fg = "#f6993f" })    -- Color for module paths in imports
+            -- vim.api.nvim_set_hl(0, "@lsp.type.interface.rust", { fg = "#f6993f" }) -- Color for module paths in imports
+            --
+            -- vim.api.nvim_set_hl(0, "@lsp.type.namespace.java", { fg = "#e5e5e5" })
+            -- vim.api.nvim_set_hl(0, "@lsp.type.class.java", { fg = "#f6993f" })
+            -- vim.api.nvim_set_hl(0, "@lsp.type.interface.java", { fg = "#f6993f" })
+            --
+            -- vim.api.nvim_set_hl(0, "Operator", { fg = "#e5e5e5" }) -- Color for module paths in imports
         end
     },
     {
